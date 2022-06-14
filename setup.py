df4da4c9214a4f3f419307d77fa30cb882517f37
Donggeun Tak <dtak@Donggeuns-MBP.fritz.box>
2022-06-15 01:20:41 +0200

    add

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(name='vtspy',
       install_requires=['astropy',
                         'numpy',
                         'matplotlib',
-
                         ],
       extras_require={'tests': ['pytest', 'pytest-ordering'],
                       'examples': ['ipywidgets', 'ipympl', 'nodejs']
